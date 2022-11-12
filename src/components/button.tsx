1db490f5b553e2b6ed61e20cab55d990ed3e1859
jooooonis <xowns3213@naver.com>
2022-11-12 19:08:31 +0900

    feat : disabled props button에 disabled props을 추가했습니다.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,25 @@
 interface ButtonProps {
   text: string;
+  disabled?: boolean;
   [key: string]: any;
 }
 
-export default function Button({ text, ...rest }: ButtonProps) {
-  return (
+export default function Button({
+  text,
+  disabled = false,
+  ...rest
+}: ButtonProps) {
+  return disabled ? (
     <button
       {...rest}
-      className="w-full h-[40px] text-headline rounded-lg border border-transparen hover:bg-sc-org-2 bg-sc-org-1 px-4 font-el text-white shadow-sm"
+      className="w-full cursor-not-allowed opacity-30 h-[40px] text-headline rounded-lg border border-transparen hover:bg-sc-org-2 bg-sc-org-1 px-4 font-el text-white"
+    >
+      {text}
+    </button>
+  ) : (
+    <button
+      {...rest}
+      className="w-full hover:cursor-pointer h-[40px] text-headline rounded-lg border border-transparen hover:bg-sc-org-2 bg-sc-org-1 px-4 font-el text-white"
     >
       {text}
     </button>
