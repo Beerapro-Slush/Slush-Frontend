80050f8f316a031891b4b56c3262fea0c30e84c2
jooooonis <xowns3213@naver.com>
2022-11-19 17:00:30 +0900

    chore : routing signup/signup01 로 변경합니다.

--- src/pages/signup/signup01.tsx
+++ src/pages/signup/signup01.tsx
@@ -35,7 +35,7 @@ export default function Signup() {
         <br /> 동의해주세요.
       </h1>
       <form autoComplete="off" className="space-y-4">
-        <div className="mt-[32px]">
+        <div className="mt-8">
           <Checkbox
             id="selectAll"
             text="모두동의 (선택사항 포함)"
@@ -43,10 +43,10 @@ export default function Signup() {
             handler={(e) => onCheckedAll(e.target.checked)}
           />
         </div>
-        <div className="mx-[8px]">
+        <div className="mx-2">
           <div className="w-full h-[1px] bg-sc-grays-6 my-[20px]"></div>
           <ul className="mb-64">
-            <li className="mb-[8px]">
+            <li className="mb-2">
               <Checkbox
                 id="term1"
                 text="만 14세 이상"
@@ -54,25 +54,25 @@ export default function Signup() {
                 handler={(e) => onChecked(e.target.checked, e.target.id)}
               />
             </li>
-            <li className="mb-[8px] flex relative">
+            <li className="mb-2 flex relative">
               <Checkbox
                 id="term2"
                 text="이용약관 동의"
                 isChecked={checkedTerm.includes('term2') ? true : false}
                 handler={(e) => onChecked(e.target.checked, e.target.id)}
               />
-              <button className="absolute bottom-[1px] right-[154px] ml-[8px] text-caption1 font-extralight underline decoration-sc-grays-1">
+              <button className="absolute bottom-[1px] right-[154px] ml-2 text-caption1 font-extralight underline decoration-sc-grays-1">
                 보기
               </button>
             </li>
-            <li className="mb-[8px] flex relative">
+            <li className="mb-2 flex relative">
               <Checkbox
                 id="term3"
                 text="개인정보 처리방침 동의"
                 isChecked={checkedTerm.includes('term3') ? true : false}
                 handler={(e) => onChecked(e.target.checked, e.target.id)}
               />
-              <button className="absolute bottom-[1px] right-[104px] ml-[8px] text-caption1 font-extralight underline decoration-sc-grays-1">
+              <button className="absolute bottom-[1px] right-[104px] ml-2 text-caption1 font-extralight underline decoration-sc-grays-1">
                 보기
               </button>
             </li>
