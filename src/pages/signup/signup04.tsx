5fd01fbd7b009d061000003bcacc1120aa4a9b5e
jooooonis <xowns3213@naver.com>
2022-12-04 02:17:16 +0900

    feat : email confirm page 이메일 인증 과정을 router를 바꿔가며 진행하기로 하였습니다.

--- src/pages/signup/signup04.tsx
+++ src/pages/signup/signup04.tsx
@@ -2,70 +2,64 @@ import Button from '@components/common/button';
 import Layout from '@components/common/layout';
 import ProgressBar from '@components/common/progressBar';
 import Input from '@components/common/input';
+import { useAppDispatch, useAppSelector } from 'redux/hooks';
+import { useForm } from 'react-hook-form';
+import { setEmail } from 'redux/slices/userSlice';
+import ErrorMessage from '@components/common/errorMessage';
+import { useRouter } from 'next/router';
+
+interface SignupForm {
+  confirm: number;
+}
 
 export default function Signup() {
+  const emailState = useAppSelector((state) => state.user.email);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<SignupForm>();
+
+  const router = useRouter();
+
+  const onValid = (form: SignupForm) => {
+    console.log(form);
+    router.push('/signup/signup05');
+  };
+
+  const watchFields = watch();
+
   return (
     <Layout>
-      <ProgressBar totalPage={5} currentPage={4} />
+      <ProgressBar totalPage={5} currentPage={3} />
       <h1 className="text-title3 font-medium">
-        회원정보를 <br />
-        입력해주세요.
+        아이디로 사용될 이메일을 <br />
+        입력 후 인증해주세요.
       </h1>
-      <form autoComplete="off" className="mt-10 ">
-        <div className="grid grid-cols-1 gap-5">
-          <Input
-            name="nickname"
-            isLabel
-            labelText="닉네임"
-            placeholder="영문, 한글, 숫자 혼합 최대 12자 가능"
-          />
-          <Input
-            name="password"
-            isLabel
-            labelText="비밀번호"
-            placeholder="영대소문자, 숫자 최소 8자 이상"
-          />
-          <Input
-            name="confirmPassword"
-            isLabel
-            labelText="비밀번호 확인"
-            placeholder="비밀번호 재입력"
-          />
-          <Input
-            name="gender"
-            isLabel
-            labelText="성별"
-            placeholder="성별 선택"
-          />
-        </div>
-        <div className="text-sc-err text-caption2 flex items-center mt-[6px]">
-          <svg
-            width="14"
-            height="14"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12 10.75C12.4142 10.75 12.75 11.0858 12.75 11.5V16.5C12.75 16.9142 12.4142 17.25 12 17.25C11.5858 17.25 11.25 16.9142 11.25 16.5V11.5C11.25 11.0858 11.5858 10.75 12 10.75Z"
-              fill="#BA1E45"
+      <form onSubmit={handleSubmit(onValid)} className="mt-10">
+        <div className="grid grid-cols-1 gap-4">
+          <div>
+            <Input disabled name="email" type="text" placeholder={emailState} />
+          </div>
+          <div>
+            <Input
+              name="confirm"
+              register={register('confirm', {
+                required: true,
+              })}
+              placeholder="입력하신 메일로 전송된 번호를 입력해주세요."
             />
-            <path
-              d="M12 9C12.5523 9 13 8.55228 13 8C13 7.44772 12.5523 7 12 7C11.4477 7 11 7.44772 11 8C11 8.55228 11.4477 9 12 9Z"
-              fill="#BA1E45"
-            />
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M3.25 12C3.25 7.16751 7.16751 3.25 12 3.25C16.8325 3.25 20.75 7.16751 20.75 12C20.75 16.8325 16.8325 20.75 12 20.75C7.16751 20.75 3.25 16.8325 3.25 12ZM12 4.75C7.99594 4.75 4.75 7.99594 4.75 12C4.75 16.0041 7.99594 19.25 12 19.25C16.0041 19.25 19.25 16.0041 19.25 12C19.25 7.99594 16.0041 4.75 12 4.75Z"
-              fill="#BA1E45"
-            />
-          </svg>
-          <span className="pl-[2px]">전송 된 번호는 10분간 유효합니다.</span>
+            <ErrorMessage message="전송 된 번호는 10분간 유효합니다." />
+          </div>
         </div>
 
-        <div className="mt-[130px]">
-          <Button text="슬러쉬 시작하기" />
+        <div className="mt-8">
+          <Button
+            disabled={!watchFields.confirm || Boolean(errors.confirm)}
+            text="남은 시간"
+          />
         </div>
       </form>
     </Layout>
