f98ce419553e530c5af323775b686250e62dc083
jooooonis <xowns3213@naver.com>
2022-12-04 02:15:05 +0900

    chore : button fontWeight

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -13,14 +13,14 @@ export default function Button({
     <button
       {...rest}
       disabled
-      className="w-full transition cursor-not-allowed opacity-30 h-[40px] text-headline rounded-lg border border-transparen hover:bg-sc-org-2 bg-sc-org-1 px-4 text-white"
+      className="w-full transition font-extralight cursor-not-allowed opacity-30 h-10 text-headline rounded-lg border border-transparen hover:bg-sc-org-2 bg-sc-org-1 px-4 text-white"
     >
       {text}
     </button>
   ) : (
     <button
       {...rest}
-      className="w-full transition hover:cursor-pointer h-[40px] text-headline rounded-lg border border-transparen hover:bg-sc-org-2 bg-sc-org-1 px-4 text-white"
+      className="w-full transition font-extralight hover:cursor-pointer h-10 text-headline rounded-lg border border-transparen hover:bg-sc-org-2 bg-sc-org-1 px-4 text-white"
     >
       {text}
     </button>
